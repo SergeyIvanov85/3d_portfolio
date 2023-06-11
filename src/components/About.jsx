a809c99072a0fe129367575ac1397ef796ef6795
Sergey Ivanov <koldyn85@yandex.ru>
2023-06-11 19:53:11 +0300

    finish: hero section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,29 @@ import { motion } from "framer-motion";
 import {styles } from "../style.js";
 import { services } from "../constants/index.js";
 import { fadeIn, textVariant } from "../utils/motion.js";
+import {SectionWrapper} from '../hoc/index.js'
 
 const ServiceCard = ({index, title, icon}) => {
   return (
-    <p>{title}</p>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title}  className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+        </div>
+
+      </motion.div>
+    </Tilt>
   )
 }
 
@@ -38,4 +57,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about");
