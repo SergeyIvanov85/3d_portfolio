94bbc58da010c4310f12d0b6360534643159f7f1
Sergey Ivanov <koldyn85@yandex.ru>
2023-06-11 18:02:04 +0300

    start about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {Tilt} from "react-tilt";
+import { motion } from "framer-motion";
+
+import {styles } from "../style.js";
+import { services } from "../constants/index.js";
+import { fadeIn, textVariant } from "../utils/motion.js";
+
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <p>{title}</p>
+  )
+}
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p variants={fadeIn("", "", 0.1, 1)}
+      className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
   )
 }
 
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,7 +4,7 @@ import  { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from '../Loader';
 
-const Computers = () => {
+const Computers = ( isMobile ) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <mesh>
@@ -20,14 +20,38 @@ const Computers = () => {
       />
       <primitive
       object={computer.scene}
-      scale={0.75}
-      position={[0, -3.25, -1.5]}
+      scale={isMobile ? 0.7 : 0.75}
+      position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
       rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
 };
 const ComputersCanvas = () => {
+
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
@@ -41,7 +65,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
